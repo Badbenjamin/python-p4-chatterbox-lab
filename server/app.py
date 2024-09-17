a26b2f4544c03261e419050829a0e870e94b5b8b
ben mccarthy <bmccarthy90@gmail.com>
2024-09-17 17:14:25 -0400

    tests pass

--- server/app.py
+++ server/app.py
@@ -14,13 +14,53 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        msgs = Message.query.all()
+        # msgs is a list of message objects
+        # needs to be serialized to dict
+        return [msg.to_dict() for msg in msgs], 200
+    elif request.method == 'POST':
+        # get the json data from request
+        # build a new message object and save to db
+        data = request.get_json()
+        new_msg = Message(
+            # .get() returns key or null if key doesn't exist. supports nested dicts or lists by chaining on .gets
+            body=data.get('body'),
+            username=data.get('username')
+        )
 
-@app.route('/messages/<int:id>')
+        db.session.add(new_msg)
+        db.session.commit()
+    
+    return new_msg.to_dict(), 201
+
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    msg = Message.query.filter(Message.id == id).first()
+
+    if msg is None:
+        return {'error' : 'message not found'}, 404
+    
+    # we need to get the data from the request
+    if request.method == 'PATCH':
+        data = request.get_json()
+
+        # just updating one field, doesn't need setattr()
+        if 'body' in data:
+            msg.body = data['body']
+
+        db.session.add(msg)
+        db.session.commit()
+
+        return msg.to_dict(), 200
+    elif request.method == 'DELETE':
+        db.session.delete(msg)
+        db.session.commit()
+
+        return {}, 204
 
 if __name__ == '__main__':
     app.run(port=5555)
